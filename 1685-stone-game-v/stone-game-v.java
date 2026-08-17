735a352ac58693e5a12d316c5dfb0cae19edc914
Sarthak Sharma <sharmasarthak934@gmail.com>
2026-08-17 11:05:16 +0530

    Time: 30 ms (95.67%) | Memory: 53 MB (5.05%) - LeetSync

--- 1685-stone-game-v/stone-game-v.java
+++ 1685-stone-game-v/stone-game-v.java
@@ -1,49 +1,84 @@
 class Solution {
-
-    int[][] f;
-
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
-        f = new int[n][n];
-        return dfs(stoneValue, 0, n - 1);
-    }
 
-    public int dfs(int[] stoneValue, int left, int right) {
-        if (left == right) {
-            return 0;
-        }
-        if (f[left][right] != 0) {
-            return f[left][right];
+        long[] prefix = new long[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + stoneValue[i];
         }
 
-        int sum = 0;
-        for (int i = left; i <= right; ++i) {
-            sum += stoneValue[i];
+        int[][] dp = new int[n][n];
+
+        int[][] leftBest = new int[n][n];
+
+        int[][] rightBest = new int[n][n];
+
+        int[] leftPtr = new int[n];
+
+        int[] rightPtr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            leftBest[i][i] = stoneValue[i];
+            rightBest[i][i] = stoneValue[i];
+
+            leftPtr[i] = i - 1;
+
+            rightPtr[i] = i;
         }
-        int suml = 0;
-        for (int i = left; i < right; ++i) {
-            suml += stoneValue[i];
-            int sumr = sum - suml;
-            if (suml < sumr) {
-                f[left][right] = Math.max(
-                    f[left][right],
-                    dfs(stoneValue, left, i) + suml
+
+        for (int len = 2; len <= n; len++) {
+            for (int l = 0; l + len <= n; l++) {
+                int r = l + len - 1;
+
+                long total = prefix[r + 1] - prefix[l];
+
+                while (leftPtr[l] + 1 <= r - 1) {
+                    int k = leftPtr[l] + 1;
+                    long leftSum = prefix[k + 1] - prefix[l];
+
+                    if (2 * leftSum > total) {
+                        break;
+                    }
+
+                    leftPtr[l]++;
+                }
+
+                while (rightPtr[l] <= r - 1) {
+                    int k = rightPtr[l];
+                    long leftSum = prefix[k + 1] - prefix[l];
+
+                    if (2 * leftSum >= total) {
+                        break;
+                    }
+
+                    rightPtr[l]++;
+                }
+
+                int best = 0;
+
+                if (leftPtr[l] >= l) {
+                    best = leftBest[l][leftPtr[l]];
+                }
+
+                if (rightPtr[l] <= r - 1) {
+                    best = Math.max(best, rightBest[rightPtr[l] + 1][r]);
+                }
+
+                dp[l][r] = best;
+
+                leftBest[l][r] = Math.max(
+                    leftBest[l][r - 1],
+                    dp[l][r] + (int) total
                 );
-            } else if (suml > sumr) {
-                f[left][right] = Math.max(
-                    f[left][right],
-                    dfs(stoneValue, i + 1, right) + sumr
-                );
-            } else {
-                f[left][right] = Math.max(
-                    f[left][right],
-                    Math.max(
-                        dfs(stoneValue, left, i),
-                        dfs(stoneValue, i + 1, right)
-                    ) + suml
+
+                rightBest[l][r] = Math.max(
+                    rightBest[l + 1][r],
+                    dp[l][r] + (int) total
                 );
             }
         }
-        return f[left][right];
+
+        return dp[0][n - 1];
     }
 }
